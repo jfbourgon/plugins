903af78e214ca5f8f033a5e48637fbf182c92a58
gendronb <bruno.gendron.consult@gmail.com>
2020-03-12 12:34:49 -0400

    Added icon to refresh button

--- elevation/templates.ts
+++ elevation/templates.ts
@@ -105,7 +105,14 @@ export const PROFILE_INFO_PANEL_TEMPLATE = ` 
 
         </div>
 
-        <md-button class="md-primary" id="rv-elevation-refresh-btn" ng-click="refresh()" ng-disabled="!isDirty">{{ 'plugins.elevation.infoPanel.refreshChartBtn.label' | translate }}</md-button>
+        <md-button class="md-primary" id="rv-elevation-refresh-btn" ng-click="refresh()" ng-disabled="!isDirty">
+            <md-icon style="position: relative; top: -2px;">
+                <svg xmlns="http://www.w3.org/2000/svg" fit height="100%" width="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" focusable="false">
+                    <g><g><g><path d="M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z"/></g></g></g>
+                </svg>
+            </md-icon>
+            {{ 'plugins.elevation.infoPanel.refreshChartBtn.label' | translate }}
+        </md-button>
 
         <md-progress-linear md-mode="indeterminate" ng-disabled="status !== 'loading'"></md-progress-linear>
 
@@ -241,7 +248,14 @@ export const STATISTICS_INFO_PANEL_TEMPLATE = ` 
 
         </div>
 
-        <md-button class="md-primary" id="rv-elevation-refresh-btn" ng-click="refresh()" ng-disabled="!isDirty">{{ 'plugins.elevation.infoPanel.refreshStatsBtn.label' | translate }}</md-button>
+        <md-button class="md-primary" id="rv-elevation-refresh-btn" ng-click="refresh()" ng-disabled="!isDirty">
+            <md-icon style="position: relative; top: -2px;">
+                <svg xmlns="http://www.w3.org/2000/svg" fit height="100%" width="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" focusable="false">
+                    <g><g><g><path d="M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z"/></g></g></g>
+                </svg>
+            </md-icon>
+            {{ 'plugins.elevation.infoPanel.refreshStatsBtn.label' | translate }}
+        </md-button>
 
         <md-progress-linear md-mode="indeterminate" ng-disabled="status !== 'loading'"></md-progress-linear>
 
@@ -303,7 +317,14 @@ export const VIEWSHED_INFO_PANEL_TEMPLATE = ` 
 
       </div>
 
-      <md-button class="md-primary" id="rv-elevation-refresh-btn" ng-click="refresh()" ng-disabled="!isDirty">{{ 'plugins.elevation.infoPanel.refreshViewshedBtn.label' | translate }}</md-button>
+      <md-button class="md-primary md-icon" id="rv-elevation-refresh-btn" ng-click="refresh()" ng-disabled="!isDirty">
+        <md-icon style="position: relative; top: -2px;">
+            <svg xmlns="http://www.w3.org/2000/svg" fit height="100%" width="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" focusable="false">
+                <g><g><g><path d="M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z"/></g></g></g>
+            </svg>
+        </md-icon>
+        {{ 'plugins.elevation.infoPanel.refreshViewshedBtn.label' | translate }}
+      </md-button>
 
       <md-progress-linear md-mode="indeterminate" ng-disabled="status !== 'loading'"></md-progress-linear>
 
